0faa2cbc99f62043e71a99151124c749ca7f7c47
semayank <setiyamayank30@gmail.com>
2024-03-08 00:26:32 +0530

    milonga text updated

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "umakanth854@gmail.com";
+$to = "mayank.setiya@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: milonga.com" . "\r\n" .
